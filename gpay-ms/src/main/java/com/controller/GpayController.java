79d9bad0a5de529e365ced0da983863124851a97
Farrux Temurov <tfarrux70@gmail.com>
2025-03-28 07:04:15 +0500

    gpay-ms added with bean, controller layers

---
+++ gpay-ms/src/main/java/com/controller/GpayController.java
@@ -0,0 +1,38 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bean.Gpay;
+import com.service.GpayService;
+
+@RestController
+@RequestMapping("gpay") // http://localhost:8484/gpay/*
+public class GpayController {
+
+	@Autowired
+	GpayService gpayService;
+
+	// http://localhost:8484/gpay/create
+	// 1st option {"emailid":"lex@gmail.com","accnum":1125} no interaction with
+	// account micro service
+
+	// 2nd option {"emailid":"lex@gmail.com"}
+	@PostMapping(value = "create", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String createAccount(@RequestBody Gpay gpay) {
+		return gpayService.createAccount(gpay);
+	}
+
+	// http://localhost:8484/gpay/findBalance/steven@gmail.com
+	@GetMapping(value = "findBalance/{emailid}")
+	public String findBalance(@PathVariable("emailid") String emailid) {
+		return gpayService.findBalance(emailid);
+
+	}
+}
