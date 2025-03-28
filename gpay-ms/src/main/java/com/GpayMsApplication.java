4abe461f2151a0c4259da51005c86d2f388a47ac
Farrux Temurov <tfarrux70@gmail.com>
2025-03-28 07:13:28 +0500

    updated properites file

--- gpay-ms/src/main/java/com/GpayMsApplication.java
+++ gpay-ms/src/main/java/com/GpayMsApplication.java
@@ -2,12 +2,28 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EntityScan(basePackages = "com.bean")
+@EnableJpaRepositories(basePackages = "com.repository")
+@EnableDiscoveryClient
 public class GpayMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GpayMsApplication.class, args);
 	}
 
+	@Bean // if we use @LoadBalance annotation, we can call other micro services by name
+			
+	@LoadBalanced 
+	public RestTemplate restTemplate() {
+		System.out.println("Rest Template object created..");
+		return new RestTemplate();
+	}
 }
